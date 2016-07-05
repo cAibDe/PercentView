81fc3bcc09c99349dc90d09fc895464defa69889
robu <robu@wangzhw-pro.local>
2016-07-05 13:59:09 +0800

    Initial Commit

---
+++ PercentView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PercentView
+//
+//  Created by robu on 16/7/5.
+//  Copyright © 2016年 robu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PercentView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PercentView
+//
+//  Created by robu on 16/7/5.
+//  Copyright © 2016年 robu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
